a6e6f3871ca096fab4bb3a8c99ef88585f33425e
Annie Shin and Rita Bennett-Chew <rita@>
2018-08-01 14:36:30 -0700

    Pass 2 tests, including areLettersVowels

--- src/word.js
+++ src/word.js
@@ -3,25 +3,42 @@ class Word {
     this.word = word;
   }
 
-  countSyllables() {
-    const vowels = ["a", "e", "i", "o", "u"];
-    const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
-    const y = ['y'];
+  countVowels() { // counts instances of the 5 vowels. Does not check for 'y's
+    const vowels = 'aeiou'
+    const consonants = "bcdfghjklmnpqrstvwxyz"
+    const y = 'y'
     let letters = this.word.split("");
 
-
-    if (!(letters.includes(y[0]))) {
-      let syllable_count = 0;
-      for (let i = 0; i < letters.length; ++i) {
-        for (let j = 0; j < vowels.length; ++j) {
-          if (letters[i] === vowels[j]) {
-            console.log(letters[i]);
-            syllable_count += 1;
-          }
-        }
+    let vowel_count = 0;
+    for (let i = 0; i < letters.length; ++i) {
+      if (vowels.includes(letters[i])) {
+        vowel_count += 1;
       }
-      return syllable_count;
+      // for (let j = 0; j < vowels.length; ++j) {
+        // if (letters[i] === vowels[j]) {
     }
+    // for each letter in letters, if it is a vowel,
+    // push a true, else push false
+    return vowel_count
+  }
+
+  countSyllables() {
+    const vowels = 'aeiou'
+    const consonants = "bcdfghjklmnpqrstvwxyz"
+    const y = 'y'
+    let letters = this.word.split("");
+
+    let syllable_count = this.countVowels()
+
+    let areLettersVowels = []
+    for (let i = 0; i < letters.length; ++i) {
+      if (vowels.includes(letters[i])) {
+        areLettersVowels.push(true)
+      } else if (consonants.includes(letters[i])) {
+        areLettersVowels.push(false)
+      }
+    }
+    return areLettersVowels
   }
 }
 
